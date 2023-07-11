3563ffe874f40db0424ef736f94b898f1acc3fe0
montasir opi <montasiropi@gmail.com>
2023-07-11 12:30:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+import Content from './component/Content';
+import Footer from './component/Footer';
+import Header from './component/Header';
+
+const App = () => {
+  return (
+    
+    <div className="bg-slate-600">
+
+    <Header  />
+    <Content />
+    <Footer />
+   
+
+    </div>
+  );
+};
+
+export default App;
+
+
---
+++ src/component/Footer.jsx
@@ -0,0 +1,25 @@
+
+
+function Footer() {
+  return (
+   <>
+   
+   <footer
+  className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left p-4 flex-grow">
+  <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
+  <strong>J. Robert Oppenheimer:</strong> Now I am become Death, the destroyer of worlds
+    <a
+      className="text-neutral-800 dark:text-neutral-400"
+      href="#"
+      > <br/>© 2023 Copyright: Fahim Montasir Opi</a
+    >
+  </div>
+</footer>
+
+  
+
+   </>
+  );
+}
+
+export default Footer;
